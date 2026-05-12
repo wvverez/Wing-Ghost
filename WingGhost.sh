ec9162b67d9f2fe51602e11aded6322bb5b4ee30
wvverez <wvverez@gmail.com>
2026-05-12 18:35:59 +0200

    Update WingGhost.sh

--- WingGhost.sh
+++ WingGhost.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # https://github.com/wvverez
- 
+
 R='\033[0;31m' G='\033[0;32m' C='\033[0;36m' X='\033[0m' B='\033[1m' Y='\033[1;33m' P='\033[38;5;129m'
 
-echo -e "${P}Versión: 1.1 Author: Wvverez | Contributions: @JVJIXFMCQ=${X}"
+echo -e "${P}Versión: 1.2 Author: Wvverez | Contributions: @JVJIXFMCQ=${X}"
 
 echo -e "${R}
 ⢀⣤⣤⣤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣤⣤⣤⣀
@@ -24,12 +24,12 @@ echo -e "${R}
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠿⠟⠿⢿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⡞⠿⠛⠿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⠁⠀⠀⠀⠈⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠀⠀⠀⠈⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀${X}"
- 
+
 sleep 0.5
- 
+
 DONE=0
 text="[+] Deleting all logs..."
- 
+
 animate() {
     local i p l
     for i in {1..35}; do
@@ -45,29 +45,43 @@ animate() {
     done
     echo -ne "\r\033[K${G}${B}[+] Deleting all logs... Ready${X}\n"
 }
- 
+
 clean_logs() {
-    command -v shred &>/dev/null || { sudo apt-get update -qq && sudo apt-get install coreutils -y -qq; } 2>/dev/null
+    command -v shred &>/dev/null || {
+        apt-get update -qq 2>/dev/null
+        apt-get install coreutils -y -qq 2>/dev/null
+    }
+
     echo -e "${G}${B}[+] Shred Active${X}"
+
     local OPTS="-f -z -n 3 -u"
-    find /var/log /home /root -type f \( -name "wtmp" -o -name "btmp" -o -name "lastlog" \
-        -o -name "*.log" -o -name ".bash_history" -o -name ".zsh_history" \) \
+
+    find /var/log /home /root \
+        -xdev \
+        \( -path /proc -o -path /sys -o -path /dev -o -path /run \) -prune -o \
+        -type f \
+        \( -name "wtmp" -o -name "btmp" -o -name "lastlog" \
+        -o -name "*.log" -o -name "*.log.*" \
+        -o -name ".bash_history" -o -name ".zsh_history" \) \
         -exec shred $OPTS {} \; 2>/dev/null
-    find / -name "*\.log\.*" -exec shred $OPTS {} \; 2>/dev/null
+
     DONE=1
 }
- 
+
 echo -e "${G}${B}[*] Cleaning logs...${X}"
+
 if [[ $EUID -ne 0 ]]; then
     echo -e "${R}[+] Not running as root${X}"
-    read -p "Press ENTER to continue or Ctrl+C to cancel: "
+    read -rp "Press ENTER to continue or Ctrl+C to cancel: "
 else
-    animate & clean_logs; wait
+    animate & clean_logs
+    wait
 fi
- 
+
 history -c 2>/dev/null
-> ~/.bash_history 2>/dev/null
-> ~/.zsh_history 2>/dev/null
- 
+: > ~/.bash_history 2>/dev/null
+: > ~/.zsh_history 2>/dev/null
+
 echo -e "${G}${B}[+] All logs deleted successfully!${X}\n"
-rm -f -- "$0"
+
+[[ -f "$0" ]] && rm -f -- "$0" 2>/dev/null
